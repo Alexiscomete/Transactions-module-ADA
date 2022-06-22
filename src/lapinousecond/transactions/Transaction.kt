1025a4ff66cde80a91b0be235e0bc246b8fb1bdf
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-06-22 11:04:47 +0200

    suite transaction

--- src/lapinousecond/transactions/Transaction.kt
+++ src/lapinousecond/transactions/Transaction.kt
@@ -3,8 +3,8 @@ package lapinousecond.transactions
 import io.github.alexiscomete.lapinousecond.entity.Owner
 import io.github.alexiscomete.lapinousecond.resources.Resource
 
-open class Transaction(val owner0: Owner, val owner1: Owner?, val amount0: Double?, val ressource0: Resource?, val amount1: Double?, val ressource1: Resource?) {
-    fun make() {
+open class Transaction(var owner0: Owner, val owner1: Owner?, val amount0: Double?, val ressource0: Resource?, val amount1: Double?, val ressource1: Resource?) {
+    open fun make() {
         if (owner1 == null || amount0 == null || ressource0 == null || amount1 == null || ressource1 == null) {
             throw IllegalArgumentException("Certains paramètres de la transaction sont invalides")
         } else {
--- src/lapinousecond/transactions/TransactionWithVerification.kt
+++ src/lapinousecond/transactions/TransactionWithVerification.kt
@@ -9,4 +9,30 @@ open class TransactionWithVerification(
     owner0, owner1, amount0, ressource0, amount1, ressource1
 ) {
 
+    // je dois vérifier que les transactions ont été acceptées par les joueurs avant de pouvoir les valider
+    override fun make() {
+        if (isValid()) {
+            super.make()
+        } else {
+            throw IllegalStateException("Les propriétaires n'ont pas accepté les transactions")
+        }
+    }
+
+    // vérifier la validité des transactions
+    fun isValid(): Boolean {
+        return isValid0 && isValid1
+    }
+
+    // demander aux propriétaires de valider la transaction si ce n'est pas déjà fait
+    fun askValidation() {
+        if (owner1 == null || amount0 == null || ressource0 == null || amount1 == null || ressource1 == null) {
+            throw IllegalStateException("Les transactions doivent avoir des propriétaires, des montants et des ressources avant d'être validées")
+        }
+        if (!isValid0) {
+            owner0.askValidation(owner1, amount0, ressource0, amount1, ressource1) { isValid0 = it }
+        }
+        if (!isValid1) {
+            owner1.askValidation(owner0, amount1, ressource1, amount0, ressource0) { isValid1 = it }
+        }
+    }
 }
